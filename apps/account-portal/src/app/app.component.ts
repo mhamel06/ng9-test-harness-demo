c8e44e3901acc4f86ce9dfb0688fe773017873b8
Matthew Hamel <mhamel@contractorsprecast.com>
2020-02-11 03:21:57 +0000

    initial commit

---
+++ apps/account-portal/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng9-comp-harness-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'account-portal';
+}
